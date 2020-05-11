4fc0d6c7b3f926e6b6a20c624000f0fcb4f8dfc7
George Cave <32631912+gcsalzburg@users.noreply.github.com>
2020-05-11 20:11:51 +0200

    Add Arduino sketches and layouts

---
+++ Arduino_sketches/01_Tilt_Switch/01_Tilt_Switch.ino
@@ -0,0 +1,19 @@
+// Pin to connect tilt switch to
+#define TILT_PIN 5
+
+void setup() { 
+   Serial.begin(115200);
+
+   // Declare tilt switch pin like a normal button
+   pinMode(TILT_PIN, INPUT_PULLUP);
+}
+
+void loop() {
+   // Tilt-switch turns "off" when it is upside down
+   if(digitalRead(TILT_PIN) == HIGH){
+      Serial.println("UPSIDE DOWN");
+   }else{
+      Serial.println("NORMAL");
+   }
+   delay(200);
+}
---
+++ Arduino_sketches/02_Ultrasonic_Switch/02_Ultrasonic_Switch.ino
@@ -0,0 +1,20 @@
+#include <NewPing.h>
+
+// Ultrasonic setup
+#define TRIGGER_PIN  10  
+#define ECHO_PIN     11  
+#define MAX_DISTANCE 200
+
+// Create a new ultrasonic sensor object
+NewPing my_ultrasonic(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+
+void setup() { 
+   Serial.begin(115200);
+}
+
+void loop() {
+   // Get the distance from the sensor with ping_cm()
+   uint16_t distance = my_ultrasonic.ping_cm();
+   Serial.println(distance);
+   delay(200);
+}
